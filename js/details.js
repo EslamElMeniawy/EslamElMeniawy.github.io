a8ab30258df2814743a542230dd0b5053a62a8ab
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2017-11-10 19:04:04 +0200

    Add details

--- js/details.js
+++ js/details.js
@@ -1,6 +1,36 @@
+var screen = '<div class="mdl-cell mdl-cell--6-col"><img src="{{image}}" class="screen"></div>"';
+
 $.getJSON("../json/apps.json", function(apps) {
-	console.log(apps);
-	console.log(apps[GetDataValue('app')]);
+	let app = apps[GetDataValue('app')];
+	$("#icon").attr("src", app.icon);
+	$("#title").html(app.name);
+
+	if (!app.android && !app.ios) {
+		$("#download").hide();
+	} else { 
+		if (app.android) {
+			$("#android").attr("href", app.android);
+			$("#android").show();
+		}
+
+		if (app.ios) {
+			$("#ios").attr("href", app.ios);
+			$("#ios").show();
+		}
+	}
+
+	$("#lang").html("Developed using: " + app.lang + ".");
+
+	if (app.source) {
+		$("#source").attr("href", app.source);
+		$("#source").show();
+	}
+
+	$("#details").html(app.details);
+
+	app.screens.forEach(function(url) {
+		$("#container").append(screen.replace("{{image}}", url));
+	});
 });
 
 function GetDataValue(VarSearch) {
