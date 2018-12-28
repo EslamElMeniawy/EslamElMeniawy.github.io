59a5aeef440a209e2aa704d4e28bdb8337faca88
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-12-28 09:00:54 +0200

    Edit .gitignore & Fix warning in project

--- js/details.js
+++ js/details.js
@@ -1,29 +1,28 @@
-var screen = '<div class="mdl-cell mdl-cell--6-col"><img src="{{image}}" class="screen"></div>';
+// noinspection HtmlUnknownTarget
+let screen = '<div class="mdl-cell mdl-cell--6-col"><img src="{{image}}" class="screen" alt="App image"></div>';
 
 $.getJSON("../json/apps.json", function(apps) {
 	let app = apps[GetDataValue('app')];
 	$("#icon").attr("src", app.icon);
 	$("#title").html(app.name);
 
+	// noinspection JSUnresolvedVariable
 	if (!app.android && !app.ios) {
 		$("#download").hide();
 	} else { 
 		if (app.android) {
-			$("#android").attr("href", app.android);
-			$("#android").css("display", "inline-block");
+			$("#android").attr("href", app.android).css("display", "inline-block");
 		}
 
 		if (app.ios) {
-			$("#ios").attr("href", app.ios);
-			$("#ios").css("display", "inline-block");
+			$("#ios").attr("href", app.ios).css("display", "inline-block");
 		}
 	}
 
 	$("#lang").html("Developed using: " + app.lang + ".");
 
 	if (app.source) {
-		$("#source").attr("href", app.source);
-		$("#source").css("display", "block");
+		$("#source").attr("href", app.source).css("display", "block");
 	}
 
 	$("#details").html(app.details);
@@ -33,12 +32,15 @@ $.getJSON("../json/apps.json", function(apps) {
 	});
 });
 
+/**
+ * @return {string}
+ */
 function GetDataValue(VarSearch) {
-	var SearchString = window.location.search.substring(1);
-	var VariableArray = SearchString.split('&');
-	for (var i = 0; i < VariableArray.length; i++) {
-		var KeyValuePair = VariableArray[i].split('=');
-		if (KeyValuePair[0] == VarSearch) {
+	let SearchString = window.location.search.substring(1);
+	let VariableArray = SearchString.split('&');
+	for (let i = 0; i < VariableArray.length; i++) {
+		let KeyValuePair = VariableArray[i].split('=');
+		if (KeyValuePair[0] === VarSearch) {
 			return KeyValuePair[1];
 		}
 	}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-var appTemp = '<div class="mdl-cell mdl-cell--{{width}}-col mdl-cell--4-col-tablet mdl-card mdl-shadow--4dp"><div class="mdl-card__title mdl-card--expand portfolio-blog-card-strip-bg mdl-color-text--white" style="background-image: url({{icon}});"></div><div class="mdl-card__supporting-text"><h2 class="mdl-card__title-text app-title">{{name}}</h2>{{description}}</div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--primary" href="details.html?app={{key}}">View Details</a></div></div>';
+// noinspection HtmlUnknownTarget
+// noinspection CssUnknownTarget
+let appTemp = '<div class="mdl-cell mdl-cell--{{width}}-col mdl-cell--4-col-tablet mdl-card mdl-shadow--4dp">'+
+	'<div class="mdl-card__title mdl-card--expand portfolio-blog-card-strip-bg mdl-color-text--white"'+
+	' style="background-image: url({{icon}});"></div>'+
+	'<div class="mdl-card__supporting-text">'+
+	'<h2 class="mdl-card__title-text app-title">{{name}}</h2>{{description}}</div>'+
+	'<div class="mdl-card__actions mdl-card--border">'+
+	'<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--primary" '+
+	'href="details.html?app={{key}}">View Details</a></div></div>';
 
 $.getJSON("../json/apps.json", function(apps) {
-	for (var key in apps) {
+	for (let key in apps) {
 		if (apps.hasOwnProperty(key)) {
 			let app = apps[key];
 
