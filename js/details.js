e4d1d9c8b20f28bc39fd6547e7b773b18ec421c0
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2020-08-10 14:40:12 +0200

    Finish refactor for resume, portofolio and app details & Fix styles & Add missing images

--- js/details.js
+++ js/details.js
@@ -1,47 +1,85 @@
-// noinspection HtmlUnknownTarget
-let screen = '<div class="mdl-cell mdl-cell--6-col"><img src="{{image}}" class="screen" alt="App image"></div>';
-
-$.getJSON("../json/apps.json", function(apps) {
-	let app = apps[GetDataValue('app')];
-	$("#icon").attr("src", app.icon);
-	$("#title").html(app.name);
-
-	// noinspection JSUnresolvedVariable
-	if (!app.android && !app.ios) {
-		$("#download").hide();
-	} else { 
-		if (app.android) {
-			$("#android").attr("href", app.android).css("display", "inline-block");
-		}
-
-		if (app.ios) {
-			$("#ios").attr("href", app.ios).css("display", "inline-block");
-		}
-	}
-
-	$("#lang").html("Developed using: " + app.lang + ".");
-
-	if (app.source) {
-		$("#source").attr("href", app.source).css("display", "block");
-	}
-
-	$("#details").html(app.details);
-
-	app.screens.forEach(function(url) {
-		$("#container").append(screen.replace("{{image}}", url));
-	});
+$.getJSON("../json/portfolio.json", function (apps) {
+    for (let app of apps) {
+        if (app["query_string"] === getDataValue('app')) {
+            fillAppData(app);
+            break;
+        }
+    }
 });
 
 /**
- * @return {string}
+ * Extract value of given query string from the page URL.
+ *
+ * @param {String} varSearch The query string key to extract value for.
  */
-function GetDataValue(VarSearch) {
-	let SearchString = window.location.search.substring(1);
-	let VariableArray = SearchString.split('&');
-	for (let i = 0; i < VariableArray.length; i++) {
-		let KeyValuePair = VariableArray[i].split('=');
-		if (KeyValuePair[0] === VarSearch) {
-			return KeyValuePair[1];
-		}
-	}
+function getDataValue(varSearch) {
+    let searchString = window.location.search.substring(1);
+    let variableArray = searchString.split('&');
+
+    for (let i = 0; i < variableArray.length; i++) {
+        let keyValuePair = variableArray[i].split('=');
+
+        if (keyValuePair[0] === varSearch) {
+            return keyValuePair[1];
+        }
+    }
+}
+
+/**
+ * Extract required data for display and add them to HTML.
+ *
+ * @param {JSON} app The app JSON object to extract data from.
+ */
+function fillAppData(app) {
+    $("#icon").attr("src", app["icon"]);
+    $("#title").html(app["name"]);
+    fillAppLinks(app);
+    $("#lang").html("Developed using: " + app["lang"] + ".");
+    fillAppDetails(app["details"]);
+    fillAppScreens(app["screens"]);
+}
+
+/**
+ * Extract app links data for display and add them to HTML.
+ *
+ * @param {JSON} app The app JSON object to extract data from.
+ */
+function fillAppLinks(app) {
+    let android = app["android"];
+    let ios = app["ios"];
+
+    if (!android && !ios) {
+        $("#download").hide();
+    } else {
+        if (android) {
+            $("#android").attr("href", android).css("display", "inline-block");
+        }
+
+        if (ios) {
+            $("#ios").attr("href", ios).css("display", "inline-block");
+        }
+    }
+}
+
+/**
+ * Add details to HTML for display.
+ *
+ * @param {Array} details JSON array for details.
+ */
+function fillAppDetails(details) {
+    details.forEach(function (details) {
+        $("#details").append('<p>' + details + '</p>');
+    });
+}
+
+/**
+ * Add screens to HTML for display.
+ *
+ * @param {Array} screens JSON array for screens.
+ */
+function fillAppScreens(screens) {
+    screens.forEach(function (screen) {
+        $("#container").append('<div class="mdl-cell mdl-cell--6-col"><img src="' +
+            screen + '" class="screen" alt="App image"></div>');
+    });
 }
